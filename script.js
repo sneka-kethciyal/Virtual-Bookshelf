9776a58c667fe806209ea26a1756c1e3751e68a1
sneka-kethciyal <snekakethciyal07@gmail.com>
2026-02-10 12:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-//select 
-var popupoverlay=document.querySelector(".popup-overlay")
-var popupbox=document.querySelector(".popup-box")
-var newbutton=document.getElementById("new-popup-btn")
+// popup elements
+var popupoverlay = document.querySelector(".popup-overlay");
+var popupbox = document.querySelector(".popup-box");
+var newbutton = document.getElementById("new-popup-btn");
 
-newbutton.addEventListener("click",function(){
-    popupoverlay.style.display="block"
-    popupbox.style.display="block"
-})
+// show popup
+newbutton.addEventListener("click", function () {
+  popupoverlay.style.display = "block";
+  popupbox.style.display = "block";
+});
 
+// cancel popup
+var cancelbtn = document.getElementById("cancel");
+cancelbtn.addEventListener("click", function () {
+  popupoverlay.style.display = "none";
+  popupbox.style.display = "none";
+});
 
-//select cancel
-var cancelbtn=document.getElementById("cancel")
-cancelbtn.addEventListener("click",function(event){
-    event.preventDefault()
-     popupoverlay.style.display="none"
-    popupbox.style.display="none"
-})
+// form elements
+var container = document.querySelector(".container");
+var addbutton = document.getElementById("add-btn");
+var booktitle = document.getElementById("book-title-input");
+var bookauthor = document.getElementById("book-author-input");
+var bookdescription = document.getElementById("book-description-input");
 
-//select add-btn, book-title-input,book-author-input,book-description-input,container
-var container=document.querySelector(".container")
-var addbutton=document.getElementById("add-btn")
-var booktitle=document.getElementById("book-title-input")
-var bookauthor=document.getElementById("book-author-input")
-var bookdescription=document.getElementById("book-description-input")
+// add book
+addbutton.addEventListener("click", function () {
 
-addbutton.addEventListener("click",function(event){
-    event.preventDefault()
-    var div=document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML=`<h2>${booktitle.value}</h2>
-            <h5>${bookauthor.value}</h5><br>
-            <p>${bookdescription.value}</p>
-            <button onclick="deleteitem(event)">Delete</button>`
-            container.append(div)
-            popupoverlay.style.display="none"
-            popupbox.style.display="none"
-        })
+  // basic validation
+  if (
+    booktitle.value === "" ||
+    bookauthor.value === "" ||
+    bookdescription.value === ""
+  ) {
+    alert("Please fill all fields");
+    return;
+  }
 
-function deleteitem(event){
-    event.target.parentElement.remove()
+  var div = document.createElement("div");
+  div.className = "book-container";
+
+  div.innerHTML = `
+    <h2>${booktitle.value}</h2>
+    <h5>${bookauthor.value}</h5><br>
+    <p>${bookdescription.value}</p>
+    <button onclick="deleteitem(event)">Delete</button>
+  `;
+
+  container.append(div);
+
+  // clear inputs
+  booktitle.value = "";
+  bookauthor.value = "";
+  bookdescription.value = "";
+
+  // close popup
+  popupoverlay.style.display = "none";
+  popupbox.style.display = "none";
+});
+
+// delete book
+function deleteitem(event) {
+  event.target.parentElement.remove();
 }
